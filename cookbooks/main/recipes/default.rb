bb2acb688295fd68fdb42eb825cf03a82f923af9
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-24 16:37:31 -0300

    Adding mongodb for solo instance recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+require_recipe "mongo_solo"
+
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # require_recipe "sphinx"
 
@@ -107,7 +109,7 @@
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
   # postgresql9_ltree "dbname"
-  # postgresql9_pg_trgm "dbname"  
+  # postgresql9_pg_trgm "dbname"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
   # postgresql9_postgis "dbname"
@@ -117,15 +119,15 @@
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
   # postgresql9_uuid_ossp "dbname"
-  
-  
+
+
   # 9.1 Extensions
-  # postgresql9_file_fdw "dbname" 
+  # postgresql9_file_fdw "dbname"
   # postgresql9_xml2 "dbname"
-  
+
   #Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
   # postgresql9_pg_stat_statements "todo" - Not done
-  
+
 # end
