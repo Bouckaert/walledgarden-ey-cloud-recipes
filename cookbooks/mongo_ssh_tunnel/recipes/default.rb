436ecd27c57d3be43771d9a7b6f5c88eb896270a
fltiago <fltiago@gmail.com>
2013-02-25 18:42:56 -0300

    Removing conditional of mongo_ssh_tunnel, because the instance is a solo instance.

--- cookbooks/mongo_ssh_tunnel/recipes/default.rb
+++ cookbooks/mongo_ssh_tunnel/recipes/default.rb
@@ -51,24 +51,20 @@ tunnel_vars = {
 
 # set this to match on the node[:instance_role] of the instance the tunnel
 # should be set up on
-if node[:instance_role] == 'app_master'
-
-  template "/etc/init.d/#{tunnel_name}" do
-    source "ssh_tunnel.initd.erb"
-    owner 'root'
-    group 'root'
-    mode 0755
-    variables(tunnel_vars)
-  end
-
-  template "/etc/monit.d/#{tunnel_name}.monitrc" do
-    source "ssh_tunnel.monitrc.erb"
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0644
-    variables(tunnel_vars)
-  end
-
-  execute "monit quit"
-
+template "/etc/init.d/#{tunnel_name}" do
+  source "ssh_tunnel.initd.erb"
+  owner 'root'
+  group 'root'
+  mode 0755
+  variables(tunnel_vars)
 end
+
+template "/etc/monit.d/#{tunnel_name}.monitrc" do
+  source "ssh_tunnel.monitrc.erb"
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0644
+  variables(tunnel_vars)
+end
+
+execute "monit quit"
